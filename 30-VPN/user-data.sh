2cf6e55797d38cc40347c18265990ad509a65e78
sindhurikolluri <sindhurikolluri2020@gmail.com>
2025-04-02 12:20:03 -0400

    VPN

---
+++ 30-VPN/user-data.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+admin_user=openvpn
+admin_pw=Openvpn@123
+reroute_gw=1
+reroute_dns=1
+
+#by default the reroute gateway and dns will be 0 if 1 then the traffic will be routed through VPN from IGW and dns
